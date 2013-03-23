bc4562f83c9c1e89d8448dcb0c895d9e2cf62dec
Duncan Smith <batfart@gmail.com>
2013-03-23 10:14:42 +0000

    Don't feed the monkey

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '83c8ac70064cf5031718f9fbe47cdf9d0f7a8ab52efe535d026103f0c52e1fd5780888865589b273202f666b3a3fb3e45e4a6090f48852e5ac8f597219b394d0'
